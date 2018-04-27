0dfd91cd2f7f6f28241785eb77b7054772d191a8
liulin_think <570389384@qq.com>
2018-04-27 10:40:33 +0800

    抽象工厂模式

--- src/main/java/com/liulin/study/factory/Factory1.java
+++ src/main/java/com/liulin/study/factory/Factory1.java
@@ -1,5 +1,10 @@
 package com.liulin.study.factory;
 
+import com.liulin.study.factory.shape.Circle;
+import com.liulin.study.factory.shape.Rectangle;
+import com.liulin.study.factory.shape.Shape;
+import com.liulin.study.factory.shape.Square;
+
 /** 
  * 【工厂模式】（Factory Pattern）是 Java 中最常用的设计模式之一。 这种类型的设计模式属于创建型模式，它提供了一种创建对象的最佳方式。
  * 在工厂模式中，我们在创建对象时不会对客户端暴露创建逻辑，并且是通过使用一个共同的接口来指向新创建的对象。
@@ -37,27 +42,8 @@ public class Factory1 {
 	}
 }
 
-interface Shape {
-	void draw();
-}
 
-class Rectangle implements Shape {
-	@Override
-	public void draw() {
-		System.out.println("this is Rectangle.draw");
-	}
-}
 
-class Square implements Shape {
-	@Override
-	public void draw() {
-		System.out.println("this is Square.draw");
-	}
-}
 
-class Circle implements Shape {
-	@Override
-	public void draw() {
-		System.out.println("this is Circle.draw");
-	}
-}
+
+
