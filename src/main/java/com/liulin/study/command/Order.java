dd2c1ec580acfa5dc97786797187799d39b383ea
liulin_think <570389384@qq.com>
2018-05-09 15:19:01 +0800

    命令模式_哎,知道怎么做,但是不知道为什么要这么做...以后有时间的时候再看看这块儿吧.

---
+++ src/main/java/com/liulin/study/command/Order.java
@@ -0,0 +1,5 @@
+package com.liulin.study.command;
+
+public interface Order {
+	   void execute();
+}
---
+++ src/main/java/com/liulin/study/command/SellStock.java
@@ -0,0 +1,13 @@
+package com.liulin.study.command;
+
+public class SellStock implements Order {
+	private Stock abcStock;
+
+	public SellStock(Stock abcStock) {
+		this.abcStock = abcStock;
+	}
+
+	public void execute() {
+		abcStock.sell();
+	}
+}
